3f868e27b6a345ca21e4015ada75ec985369d775
nameartem <artseleznev@gmail.com>
2020-11-27 18:32:26 +0300

    style guide

--- config/conf.py
+++ config/conf.py
@@ -3,7 +3,7 @@ PORT = 8765
 TEMPLATES_PATH = "templates"
 STATIC_FILES_PATH = "static"
 
-#application name
+# application name
 APPNAME = "TeamForge"
 AUTHOR = "SeleznevArtem"
 DESC = "Platfom for a good smith how works with his team in forge"
